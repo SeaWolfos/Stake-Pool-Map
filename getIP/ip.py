e99231c60fc81de637734acd642d21a912c3d0d9
SeaWolfos <dallas@cmc3.me>
2020-03-14 17:57:23 +0000

    nitpicking

--- getIP/ip.py
+++ getIP/ip.py
@@ -3,13 +3,10 @@
 # BOOM BAM BABY IT WORKS
 
 import re
-from collections import Counter
 reggie = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
-f = open('../data/new.txt')
-log = f.read()
-iplist = re.findall(reggie,log)
-with open("iplog.txt", "a") as iplog:
-    for list in iplist:
-        iplog.write(list +"\n")
-        #print(list)
+with open('../data/new.txt') as f:
+    log = f.read()
+    iplist = re.findall(reggie,log)
+    with open("iplog.txt", "a") as iplog:
+        iplog.write('\n'.join(iplist))
 
