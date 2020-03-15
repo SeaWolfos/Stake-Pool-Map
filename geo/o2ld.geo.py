096a9893f07dd693fa6642537cb5c407d5c7dc64
SeaWolfos <link157@protonmail.com>
2020-03-15 09:43:25 +0000

    organizing

--- geo/o2ld.geo.py
+++ geo/o2ld.geo.py
@@ -2,6 +2,7 @@
 
 from ipdata import ipdata
 from pprint import pprint
+
 f = open('/home/cam/projects/project_secrets/geo.key', 'r')
 #print(f) # debugging#don't print API keys
 apiKey = f.readline().strip() # strips away \n at EOL
@@ -9,8 +10,8 @@ ipdata = ipdata.IPData(apiKey) # api key goes here
 
 # gonna do this manually to get everything online
 ipAsk = input("Enter IP Address: ")
-
 #ipdata = ipdata.IPData('') #api key
+
 response = ipdata.lookup(ipAsk) #ip address - this will be !manual later
-#pprint(respons open
-pprint(response)
+#pprint(response)
+with open(
